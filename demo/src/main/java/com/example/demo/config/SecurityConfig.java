9bf49e67e02ed52566d743e451ad7b4509895394
OMAR BEEN HASIB <adonomar02@gmail.com>
2026-06-24 22:35:43 +0300

    Add SecurityConfig to fix login redirect loop and restore custom login page

--- demo/src/main/java/com/example/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,5 +1,26 @@
 package com.example.demo.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
-    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+            auth -> auth
+            .antMatchers("/login", "/register","/css/**", "/js/**").permitAll()
+            .anyRequest().authenticated()
+        ).formLogin(
+            form -> form
+            .loginPage("/login")
+            .defaultSuccessUrl("/projects", true)
+            .permitAll()
+        ).logout(logout -> logout.permitAll());
+
+        return http.build();
+    }
+
 }
